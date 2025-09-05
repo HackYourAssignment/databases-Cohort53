e14a421cf8353824b512a4339869ec39c9f2d95d
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-09-05 21:00:48 +0200

    AUTHOR_QUANTITY_VIEW completed

---
+++ Week2/assignment/Ex4AggregateFunctions.js
@@ -0,0 +1,50 @@
+import { Client } from "pg";
+import { authors, researchPapers, paperAuthors } from "./generateSampleData.js";
+
+// Database connection configuration
+const config = {
+  host: "localhost",
+  user: "hyfuser",
+  password: "hyfpassword",
+  database: "DBassignmentW2",
+  port: 5432,
+};
+
+const client = new Client(config);
+let queryRes;
+
+async function createDBviews(client) {
+  try {
+    await client.connect();
+    console.log("Connected to PostgreSQL database!");
+
+    // Create a view for all research papers and the number of authors that wrote that paper.
+    const CREATE_AUTHOR_QUANTITY_VIEW = `
+      CREATE OR REPLACE VIEW paper_authors_qty AS
+      SELECT 
+        paper_title,
+        COUNT(pa.author_id) as number_of_authors
+      FROM
+        research_papers rp
+        JOIN paper_authors pa ON rp.paper_id = pa.paper_id
+        GROUP BY paper_title
+    `;
+
+    await client.query(CREATE_AUTHOR_QUANTITY_VIEW);
+    // Query the view to verify it works
+    queryRes = await client.query("SELECT * FROM paper_authors_qty");
+    console.log(
+      "\nResearch papers and the number of their authors:",
+      JSON.stringify(queryRes.rows, null, 2)
+    );
+
+    console.log("Database seeding completed successfully!");
+  } catch (error) {
+    console.error("Error seeding database:", error);
+  } finally {
+    await client.end();
+  }
+}
+
+// Execute the seeding function
+createDBviews(client);
