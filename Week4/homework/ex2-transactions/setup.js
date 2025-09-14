7392b0cbd36ee7fa325aaf1e6aac6074a3ef5e44
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-09-15 00:41:07 +0200

    collections are cleaned up before being populated

--- Week4/homework/ex2-transactions/setup.js
+++ Week4/homework/ex2-transactions/setup.js
@@ -10,8 +10,10 @@ export async function setup() {
   await clientMongo.connect();
   const db = clientMongo.db("databaseWeek4");
   const account = await db.createCollection("accounts");
+  await account.deleteMany({});
   await account.insertMany(accounts);
   const accountChanges = await db.createCollection("account_changes");
+  await accountChanges.deleteMany({});
   await accountChanges.insertMany(account_changes);
   return { clientMongo, account, accountChanges };
 }
