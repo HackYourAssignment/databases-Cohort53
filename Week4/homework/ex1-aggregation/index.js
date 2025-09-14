618f596da7a466ef38a14e090aa9160f058108f1
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-09-14 12:22:36 +0200

    ContinentPopulation completed

--- Week4/homework/ex1-aggregation/index.js
+++ Week4/homework/ex1-aggregation/index.js
@@ -36,6 +36,46 @@ async function getTotalPopulationByYear(collection, country) {
   return result;
 }
 
+async function getContinentsPopulation(collection, year, age) {
+  const result = await collection
+    .aggregate([
+      {
+        $match: {
+          Year: year.toString(),
+          Age: age.toString(),
+          // Filter only continents by checking if Country is uppercase
+          $expr: {
+            $eq: ["$Country", { $toUpper: "$Country" }],
+          },
+        },
+      },
+      {
+        $addFields: {
+          M: { $toInt: "$M" },
+          F: { $toInt: "$F" },
+        },
+      },
+      {
+        $addFields: {
+          TotalPopulation: { $add: ["$M", "$F"] },
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          Country: 1,
+          Year: 1,
+          Age: 1,
+          M: 1,
+          F: 1,
+          TotalPopulation: 1,
+        },
+      },
+    ])
+    .toArray();
+  return result;
+}
+
 async function main() {
   try {
     await clientMongo.connect();
@@ -46,7 +86,15 @@ async function main() {
     );
     await collection.insertMany(data);
     data = await getTotalPopulationByYear(collection, "Netherlands");
-    console.log(JSON.stringify(data, null, 2));
+    console.log(
+      "\nPopulation by Year (Netherlands):\n",
+      JSON.stringify(data, null, 2)
+    );
+    data = await getContinentsPopulation(collection, "2020", "100+");
+    console.log(
+      "\nPopulation by Continent (2020, 100+):\n",
+      JSON.stringify(data, null, 2)
+    );
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
   } finally {
